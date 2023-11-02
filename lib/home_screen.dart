c42cbb5d907038c9558884ffb3fc8c01af85b5a6
BrunoCura2020 <brunocura16@gmail.com>
2023-11-01 21:47:14 -0300

    Se agrega reactividad al formulario

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,36 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // creates a group
+    final form = fb.group({
+      'name': ['', Validators.required],
+      'email': ['', Validators.required, Validators.email],
+      'password': Validators.required,
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Mi AppBar'),
       ),
       body: Center(
-        child: Form(
+        child: ReactiveForm(
+          formGroup: form,
           child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  label: Text('Name'),
-                ),
+            children: <Widget> [
+              ReactiveTextField(
+                formControlName: 'name', 
+                //suffixIcon: Icon(Icons.check_circle_outline_rounded)  
               ),
-        
+         
               TextFormField(
                 decoration: const InputDecoration(
                   label: Text('Correo'),
+                  suffixIcon: Icon(Icons.cancel_outlined)
                 ),
               ),
         
-              TextFormField(
-                decoration: const InputDecoration(
-                  label: Text('Password'),
-                ),
+              ReactiveTextField(
+                formControlName: 'name', 
+                obscureText: true,
               ),
-              
+
               const SizedBox(height: 20,),
 
               FilledButton(
